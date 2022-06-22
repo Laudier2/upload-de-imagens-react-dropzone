91626f1c91259d6e06a9b54008b1bed6a08b7bbb
Laudier2 <trabalhandooline2019@hotmail.com>
2022-06-22 12:52:53 -0300

    make it better

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Container, Content } from './styles.js';
 import GloboStyle from './styles/global'
 import Imagens from './components/Upload';
 import { AppProvider } from './Context/AppContextState.js';
+import './App.scss'
+import { Button, Row, Col } from 'react-bootstrap';
 
 function App() {
 
@@ -10,9 +12,17 @@ function App() {
       <Container>
         <Content>
           <Imagens />
+          <Button>
+            Enviar
+          </Button>
         </Content>
         <GloboStyle />
       </Container>
+      <Row className="mx-0 mt-2">
+        <Button as={Col} variant="primary">Button #1</Button>
+        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+        <Button as={Col} variant="success">Button #3</Button>
+      </Row>
     </AppProvider>
   );
 }
